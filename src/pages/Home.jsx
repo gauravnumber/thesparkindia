60655374a68b079b6b078f3822c3bf045845dcbf
Gaurav Singh <gauravnumber@gmail.com>
2022-02-07 16:32:07 +0530

    sidebar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import { Grid, Menu, Icon, Input, Header } from "semantic-ui-react";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
     <div>
-      <Menu borderless>
+      <Sidebar />
+      {/* <Menu borderless>
         <Menu.Item>
           <Icon name="bars" />
         </Menu.Item>
         <Menu.Item position="right">
           <Icon name="user" /> <Icon name="shopping cart" />
         </Menu.Item>
-        {/* <Menu.Menu>
-          <Input icon="search" placeholder="Search for products" />
-        </Menu.Menu> */}
       </Menu>
-      <Input icon="search" placeholder="Search for products" fluid />
+      <Input icon="search" placeholder="Search for products" fluid /> */}
     </div>
   );
 };
