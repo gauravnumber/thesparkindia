0059987ae65e1fe886669302ddcf3fa6c69e3a36
Gaurav Singh <gauravnumber@gmail.com>
2022-02-25 20:15:59 +0530

    removing unneccessary code

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-// import { red } from "@mui/material/colors";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -13,14 +11,6 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import App from "./App";
-
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: red[500],
-//     },
-//   },
-// });
 
 ReactDOM.render(
   <React.StrictMode>
