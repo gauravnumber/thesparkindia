94adf9615d01cb4b04b5cb2456f6da4abb26e358
Gaurav Singh <gauravnumber@gmail.com>
2022-03-03 11:19:55 +0530

    enquiry list is created

--- src/pages/Enquiry.jsx
+++ src/pages/Enquiry.jsx
@@ -1,7 +1,34 @@
-import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { SearchOutlined } from "@mui/icons-material";
+import {
+  Badge,
+  Stack,
+  Box,
+  Avatar,
+  Divider,
+  IconButton,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const Enquiry = () => {
+  const enquiries = [
+    {
+      heading: "6v LED light green color",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      unReadMessage: 1,
+    },
+    {
+      heading: "Fan 3000rpm motor blue",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      unReadMessage: 6,
+    },
+  ];
+
   return (
     <>
       {" "}
@@ -24,6 +51,119 @@ const Enquiry = () => {
         }}
         fullWidth
       />
+      <List
+        sx={{
+          width: "100%",
+          //  maxWidth: 360,
+          bgcolor: "background.paper",
+        }}
+      >
+        {enquiries.map((enquiry, index) => (
+          <>
+            <ListItem
+              key={index}
+              // alignItems="flex-start"
+              secondaryAction={
+                <Badge badgeContent={enquiry.unReadMessage} color="primary" />
+              }
+            >
+              <ListItemAvatar>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://source.unsplash.com/100x100"
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={enquiry.heading}
+                secondary={enquiry.message}
+              />
+              {/* <ListItemText secondary="like" component="span" /> */}
+            </ListItem>
+            <Divider
+              //  variant="inset"
+              component="li"
+            />
+          </>
+        ))}
+        {/* <ListItem
+          key="first"
+          // alignItems="flex-start"
+          sx={
+            {
+              // ".MuiListItemSecondaryAction-root": {
+              //   borderRadius: "50%",
+              //   p: 5,
+              // },
+            }
+          }
+          secondaryAction={
+            <Badge badgeContent={3} color="primary" />
+            // <Stack direction="column" justifyContent="space-between">
+            //   <Box
+            //     component="span"
+            //     sx={{
+            //       bgcolor: "primary.main",
+            //       // "& .MuiListItemSecondaryAction-root": {
+            //       //   borderRadius: "50%",
+            //       //   p: 5,
+            //       // },
+            //       // borderRadius: "12px",
+            //       borderRadius: "50%",
+            //       p: 0.5,
+            //       height: "20px",
+            //       width: "20px",
+            //     }}
+            //   >
+            //     6
+            //   </Box>
+            //   <Box component="span" color="text.secondary" variant="caption">
+            //     8:30 pm
+            //   </Box>
+            // </Stack>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://source.unsplash.com/100x100"
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary="6v LED light green color"
+            // secondary={
+            //   <Typography>
+            //     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            //   </Typography>
+            // }
+            secondary="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+        </ListItem>
+        <Divider
+          //  variant="inset"
+          component="li"
+        /> */}
+        {/* <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Summer BBQ"
+            secondary={
+              <>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </>
+            }
+          />
+        </ListItem> */}
+      </List>
     </>
   );
 };
