7b4d90163bf654bbb125c16e8babe63ceef02785
Gaurav Singh <gauravnumber@gmail.com>
2022-03-25 14:59:31 +0530

    slide in hot deals working

--- src/components/Home/LiveStudio/Slide.jsx
+++ src/components/Home/LiveStudio/Slide.jsx
@@ -1,5 +1,5 @@
 import { IconButton, Typography, Grid, Box, Stack } from "@mui/material";
-import { PanoramaFishEye } from "@mui/icons-material";
+import { Visibility } from "@mui/icons-material";
 
 const Slide = () => {
   return (
@@ -19,32 +19,63 @@ const Slide = () => {
           sx={{
             // width: "148px",
             // width: 148,
+            p: 1,
             minWidth: 148,
-            // height: 210,
+            height: 210,
             color: (theme) => theme.palette.primary.contrastText,
             backgroundColor: "black",
             borderRadius: "3px",
             background:
-              "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 94.58%), url(.jpg);",
+              "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 94.58%), url('https://source.unsplash.com/148x210')",
+            // background: "url('https://source.unsplash.com/148x210')",
+            // backgroundImage:
+            //   "url('https://source.unsplash.com/148x210?black,background')",
+
             border: `1px solid #ff2608`,
 
             // borderImageSource:
             //   " linear-gradient(180deg, #FF2608 0%, rgba(255, 38, 8, 0) 100%)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            // alignItems: "flex-end",
           }}
         >
           <Typography
             sx={{
               fontWeight: 500,
               fontSize: (theme) => theme.typography.pxToRem(8.07),
-              color: "#d6d6d6",
+              color: "primary.grey2",
             }}
           >
             Ashu PVT
           </Typography>
-          <Typography>Best Electric trimmers in 2022 with 5 Modes</Typography>
-          <Typography>
-            <IconButton size="small">
-              <PanoramaFishEye />
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: (theme) => theme.typography.pxToRem(12),
+            }}
+          >
+            Best Electric trimmers in 2022 with 5 Modes
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: (theme) => theme.typography.pxToRem(8.07),
+              color: "primary.grey2",
+            }}
+          >
+            <IconButton
+              sx={{
+                p: 0,
+                pr: "5px",
+                color: "primary.grey2",
+                "& .MuiSvgIcon-root": {
+                  fontSize: 11,
+                },
+              }}
+            >
+              <Visibility />
             </IconButton>
             2,352 watching
           </Typography>
