50b7d67e22b6d71629e32789cc1c6cb9a5d5610c
Gaurav Singh <gauravnumber@gmail.com>
2022-03-29 21:04:12 +0530

    adjustment in recommended small section

--- src/components/Home/Recommended/index.jsx
+++ src/components/Home/Recommended/index.jsx
@@ -52,7 +52,11 @@ const Recommended = () => {
           <ListItemAvatar>
             <Avatar
               src="https://source.unsplash.com/48x46"
-              sx={{ width: 48, height: 46, borderRadius: "unset" }}
+              sx={{
+                //  width: 48,
+                height: 46,
+                borderRadius: "unset",
+              }}
             />
           </ListItemAvatar>
           <ListItemText
@@ -157,13 +161,96 @@ const Recommended = () => {
             </Typography>
           </Stack>
         </Grid>
-        <Grid item xs={5}>
-          <Box
+        <Grid item container flexDirection="column" xs={5}>
+          {/* <Box
             sx={{
               backgroundColor: "red",
               height: 100,
             }}
           />
+          <Box
+            sx={{
+              backgroundColor: "blue",
+              height: 100,
+            }}
+          /> */}
+          {Array.from(Array(2)).map((_, index) => (
+            <Stack
+              key={index}
+              direction="column"
+              alignItems="center"
+              sx={{ width: 154 }}
+            >
+              <Avatar
+                src="https://source.unsplash.com/50x50?monitor"
+                sx={{
+                  // width: 50,
+                  height: 50,
+                  borderRadius: "unset",
+                  // textAlign: "center",
+                }}
+              />
+              <Stack direction="column" spacing={1} sx={{ p: 1 }}>
+                <Typography
+                  noWrap
+                  sx={{
+                    fontSize: (theme) => theme.typography.pxToRem(13),
+                    lineHeight: (theme) => theme.typography.pxToRem(15),
+                    textOverflow: "ellipsis",
+                    // width: 206,
+                    // height: 15,
+                  }}
+                >
+                  Dell 16inch monitor 500W input monitor
+                </Typography>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  spacing="6px"
+                  sx={{ pl: "12px" }}
+                >
+                  <Typography
+                    sx={{
+                      fontWeight: 500,
+                      fontSize: (theme) => theme.typography.pxToRem(13),
+                      lineHeight: (theme) => theme.typography.pxToRem(15),
+                    }}
+                  >
+                    Rs25,000
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#656565",
+                      fontSize: (theme) => theme.typography.pxToRem(13),
+                      lineHeight: (theme) => theme.typography.pxToRem(15),
+                      textDecorationLine: "line-through",
+                    }}
+                  >
+                    30,000
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#828282",
+                      fontSize: (theme) => theme.typography.pxToRem(8.19),
+                      lineHeight: (theme) => theme.typography.pxToRem(10),
+                    }}
+                  >
+                    Per item
+                  </Typography>
+                </Stack>
+                <Typography
+                  sx={{
+                    color: "#338aff",
+                    fontSize: (theme) => theme.typography.pxToRem(11),
+                    lineHeight: (theme) => theme.typography.pxToRem(15),
+                    pl: "12px",
+                  }}
+                >
+                  For 50 units
+                </Typography>
+              </Stack>
+            </Stack>
+          ))}
         </Grid>
       </Grid>
     </Paper>
