3a7d77c9bd4b809752b14e611d03cd73434de22f
Gaurav Singh <gauravnumber@gmail.com>
2022-03-31 12:05:34 +0530

    search bar improved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,25 @@
-import FloatButton from "@/components/Home/FloatButton";
 import BackToTop from "@/components/Home/BackToTop";
-import RecentlyViewedProducts from "@/components/Home/RecentlyViewedProducts";
-import TopSellingBrands from "@/components/Home/TopSellingBrands";
-import Recommended from "@/components/Home/Recommended";
-import SparkFeatures from "@/components/Home/SparkFeatures";
-import Sponsored from "@/components/Home/Sponsored";
-import TrendingSearches from "@/components/Home/TrendingSearches";
-import TrendingProducts from "@/components/Home/TrendingProducts";
 import Categories from "@/components/Home/Categories";
+import FloatButton from "@/components/Home/FloatButton";
 import HotDeals from "@/components/Home/HotDeals";
 import LiveStudio from "@/components/Home/LiveStudio";
+import RecentlyViewedProducts from "@/components/Home/RecentlyViewedProducts";
+import Recommended from "@/components/Home/Recommended";
+import SearchBar from "@/components/Home/SearchBar";
 import Slide from "@/components/Home/Slide";
 import Spark from "@/components/Home/Spark";
+import SparkFeatures from "@/components/Home/SparkFeatures";
+import Sponsored from "@/components/Home/Sponsored";
+import TopSellingBrands from "@/components/Home/TopSellingBrands";
+import TrendingProducts from "@/components/Home/TrendingProducts";
+import TrendingSearches from "@/components/Home/TrendingSearches";
 import Img from "@/components/Img";
-import { SearchOutlined } from "@mui/icons-material";
-import { Box, IconButton, InputAdornment, TextField, Fab } from "@mui/material";
+import { Box } from "@mui/material";
 
 const Home = () => {
   return (
     <Box mx={-1}>
-      <TextField
-        size="small"
-        sx={{
-          mt: 1,
-          px: 1,
-          // borderRadius: "15px",
-          bgcolor: "background.blue2",
-          // bgcolor: "background.paper",
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <IconButton edge="start">
-                <SearchOutlined />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-        fullWidth
-      />
+      <SearchBar />
       <Img
         src="https://source.unsplash.com/393x67?advertisement,discount"
         sx={{ mt: 1, mb: -1 }}
