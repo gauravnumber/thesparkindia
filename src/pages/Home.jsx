69c0402321521157433dad79bcc27df5f1036111
Gaurav Singh <gauravnumber@gmail.com>
2022-04-08 12:15:05 +0530

    menu slide created in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import CropFreeIcon from "@mui/icons-material/CropFree";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+
 import BackToTop from "@/components/Home/BackToTop";
 import Categories from "@/components/Home/Categories";
 import FloatButton from "@/components/Home/FloatButton";
@@ -25,6 +32,7 @@ import {
   AppBar as MuiAppBar,
   Box,
   Drawer,
+  Divider,
   IconButton,
   styled,
   Toolbar,
@@ -41,8 +49,7 @@ const Home = () => {
 
   const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
     ({ theme, open }) => ({
-      // flexGrow: 1,
-      // padding: theme.spacing(3),
+      flexGrow: 1,
       transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
@@ -87,13 +94,13 @@ const Home = () => {
     <Box
       //  mt={-5}
       mx={-1}
+      // display="flex"
+      // in <Main> uncomment -drawerWidth
     >
       <AppBar
         position="fixed"
         open={open}
         sx={{
-          // color: "primary.blue",
-          // backgroundColor: (theme) => theme.palette.background.paper,
           height: 40,
           // boxShadow: (theme) => theme.shadows[1],
           "& .MuiToolbar-root": {
@@ -171,6 +178,303 @@ const Home = () => {
         >
           <Close />
         </IconButton>
+        <Box
+          sx={{
+            width: drawerWidth,
+            height: 802,
+            left: 0,
+            top: 42,
+          }}
+        >
+          <Box
+            sx={{
+              width: drawerWidth,
+              height: 124,
+              left: 0,
+              top: 0,
+              bgcolor: "#2847A1",
+              position: "relative",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "#FFFFFF",
+                position: "absolute",
+                width: 43,
+                height: 20,
+                left: 21,
+                top: 11,
+              }}
+            >
+              Hello!
+            </Typography>
+            <AccountCircleRoundedIcon
+              fontSize="large"
+              color="action"
+              sx={{
+                position: "absolute",
+                width: 56.57,
+                height: 56.57,
+                left: 17.71,
+                top: 42.71,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                width: 55,
+                height: 22,
+                left: 96,
+                top: 46,
+                color: "#FFFFFF",
+              }}
+            >
+              Junaid
+            </Typography>
+            <Typography
+              sx={{
+                position: "absolute",
+                width: 101,
+                height: 16,
+                left: 96,
+                top: 73.18,
+                color: "#FFFFFF",
+              }}
+            >
+              +919568923556
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <HomeRoundedIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Home
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <CategoryRoundedIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Category
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <VideoLibraryIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Studio
+            </Typography>
+          </Box>
+          <Divider />
+
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <CropFreeIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              My profile
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <LocalShippingIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              My orders
+            </Typography>
+          </Box>
+          <Divider />
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <HomeRoundedIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Ship with spark
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <CategoryRoundedIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Scan and pay
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <VideoLibraryIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Refer and pay
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              position: "relative",
+              width: drawerWidth,
+              height: 48,
+              left: 0,
+            }}
+          >
+            <CropFreeIcon
+              color="action"
+              sx={{
+                m: 2,
+              }}
+            />
+            <Typography
+              sx={{
+                position: "absolute",
+                left: 50,
+                top: 0,
+                m: 2,
+              }}
+            >
+              Credits and Wallet
+            </Typography>
+          </Box>
+        </Box>
       </Drawer>
       <Main open={open}>
         <SearchBar />
