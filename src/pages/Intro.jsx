23103248fdb95208a3d2937c9e33148fb95c8267
Gaurav Singh <gauravnumber@gmail.com>
2022-04-19 12:20:19 +0530

    intro page improved

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -14,75 +14,28 @@ import { Image } from "mui-image";
 
 const Intro = () => {
   const theme = useTheme();
-  // const items =[{
-
-  // }]
-  // return <Box mt={-5}>intro</Box>;
 
   return (
-    <Box mt={-5} p={1} sx={{ position: "relative" }}>
-      {/* <Button sx={{ float: "right", display: "block" }}>Skip</Button> */}
-      {/* <br /> */}
-      {/* <Button sx={{ position: "absolute", right: 0, top: 0, mb: 3 }}>
-        Skip
-      </Button> */}
-      {/* <Avatar>
-        <ArrowBack />
-      </Avatar> */}
+    <Box mt={-5} p={1}>
       <Stack direction="row" justifyContent="flex-end">
         <Button sx={{ color: "primary.darkBlue" }}>Skip</Button>
       </Stack>
       <Carousel
         // autoPlay={false}
-        // indicatorIconButtonProps={{
-        //   style: {
-        //     // padding: "10px", // 1
-        //     color: "blue", // 3
-        //   },
-        // }}
+
         activeIndicatorIconButtonProps={{
           style: {
-            color: theme.palette.primary.darkBlue, // 3
-
-            // backgroundColor: "red", // 2
+            color: theme.palette.primary.darkBlue,
           },
         }}
-
-        // navButtonsWrapperProps={{
-        //   // Move the buttons to the bottom. Unsetting top here to override default style.
-        //   style: {
-        //     color: "blue",
-        //     bottom: "0",
-        //     backgroundColor: "green",
-        //     top: "unset",
-        //   },
-        // }}
-        // // fullHeightHover={false}
-        // navButtonsProps={{
-        //   // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-        //   style: {
-        //     backgroundColor: "green",
-        //     color: "red",
-        //     borderRadius: 0,
-        //   },
-        // }}
       >
         {Array.from(Array(3)).map((_, i) => (
-          <Stack
-            key={i}
-            // sx={{ height: "500px" }}
-            direction="column"
-          >
+          <Stack key={i} direction="column">
             <Image
-              src={`https://source.unsplash.com/200x250?sig=${i}`}
+              src={`https://source.unsplash.com/200x250?family&sig=${i}`}
               alt="demo"
               height={250}
-              // width="200"
-              // variant="square"
-              // sx={{
-              //   //  width: "100%",
-              //   height: "10px",
-              // }}
+              // showLoading
             />
             <Typography
               variant="h5"
@@ -90,13 +43,11 @@ const Intro = () => {
                 fontSize: (theme) => theme.typography.pxToRem(18),
                 textAlign: "center",
                 mt: 7,
-                // mx: "34px",
               }}
             >
               One step destination of all buyers and seller's
             </Typography>
           </Stack>
-          // <Slide key={i} item={item} />
         ))}
       </Carousel>
 
