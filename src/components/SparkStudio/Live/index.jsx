54307dd8cf3bffef68b35731f960a5d898393656
Gaurav Singh <gauravnumber@gmail.com>
2022-05-07 15:19:26 +0530

    image text added

--- src/components/SparkStudio/Live/index.jsx
+++ src/components/SparkStudio/Live/index.jsx
@@ -1,5 +1,6 @@
 import { TvOutlined } from "@mui/icons-material";
 import {
+  Avatar,
   Box,
   ListItem,
   ListItemIcon,
@@ -31,14 +32,17 @@ const Live = () => {
       </ListItem>
       <Stack direction="row" spacing={1} sx={{ overflowX: "scroll", p: 1 }}>
         {Array.from(Array(10)).map((_, index) => (
-          <Box
+          <Stack
             key={index}
+            direction="column"
+            justifyContent="space-between"
             sx={{
               minWidth: 130,
               height: 165,
               // backgroundColor: "lightgreen",
               borderRadius: "10px",
               background: `linear-gradient(0deg, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url(https://source.unsplash.com/130x165?sig=${index})`,
+              overflow: "hidden",
             }}
           >
             <Stack
@@ -71,13 +75,48 @@ const Live = () => {
                     fontWeight: 600,
                   }}
                 >
-                  Live
+                  &bull; Live
                 </Typography>
               </Stack>
               <Box flexGrow={1} />
-              <Typography>one</Typography>
+              {/* <Typography>one</Typography> */}
+              <Avatar
+                src="https://source.unsplash.com/29x29?social-media-user"
+                alt="user"
+                sx={{
+                  width: 29,
+                  height: 29,
+                }}
+              />
             </Stack>
-          </Box>
+
+            {/* <Box flexGrow={1} /> */}
+            <Stack
+              direction="column"
+              sx={{
+                p: "4.67px 6.22px",
+                background: "rgba(0, 0, 0, 0.5)",
+              }}
+              spacing="3px"
+            >
+              <Typography
+                sx={{ fontSize: "12px", fontWeight: 600, color: "#fff" }}
+              >
+                Sindhu.pvt
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "9px",
+                  fontWeight: 400,
+                  color: "#fff",
+                  textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                  lineHeight: "11px",
+                }}
+              >
+                Best Selling Electronics under 2000/-
+              </Typography>
+            </Stack>
+          </Stack>
         ))}
       </Stack>
     </Box>
