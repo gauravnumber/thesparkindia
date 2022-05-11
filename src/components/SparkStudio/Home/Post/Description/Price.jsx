ea1653708497fb7482b88e37be31aa55f9e15c0a
Gaurav Singh <gauravnumber@gmail.com>
2022-05-11 19:37:22 +0530

    pricing added

--- src/components/SparkStudio/Home/Post/Description/Price.jsx
+++ src/components/SparkStudio/Home/Post/Description/Price.jsx
@@ -1,7 +1,12 @@
+import { Image } from "mui-image";
 import { Stack, Box, Typography } from "@mui/material";
 
 const Price = () => {
   const lightBlue = "#f1f7ff";
+  const priceHeadingColor = "#1c1b1f";
+  const cuttingPriceColor = "rgba(28, 27, 31,0.5)";
+  const priceOffBlueColor = "#2847a1";
+
   return (
     <Stack
       direction="row"
@@ -25,13 +30,57 @@ const Price = () => {
           // spacing={2}
         >
           <Stack direction="column" spacing="5px" sx={{ p: "10px" }}>
-            <Typography>Dell Motherboard</Typography>
+            <Typography
+              sx={{
+                fontSize: 13,
+                color: priceHeadingColor,
+                lineHeight: "20px",
+              }}
+            >
+              Dell Motherboard
+            </Typography>
+            <Stack direction="row" spacing="13px" alignItems="center">
+              <Typography
+                sx={{
+                  fontSize: 13,
+                  textDecorationLine: "line-through",
+                  color: cuttingPriceColor,
+                  lineHeight: "24px",
+                }}
+              >
+                Rs5,000
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: 13,
+                  fontWeight: 500,
+                  // textDecorationLine: "line-through",
+                  // color: cuttingPriceColor,
+                  lineHeight: "24px",
+                }}
+              >
+                Rs3000
+              </Typography>
+            </Stack>
+            <Typography
+              sx={{
+                pl: "22px",
+                fontSize: 12,
+                color: priceOffBlueColor,
+              }}
+            >
+              21%off
+            </Typography>
           </Stack>
-          <Box
+          <Image
+            src={`https://source.unsplash.com/80x88?MotherBoard&sig=${index}`}
+            width={80}
+          />
+          {/* <Box
             width={80}
             backgroundColor="green"
             backgroundImage={`https://source.unsplash.com/80x88`}
-          />
+          /> */}
         </Stack>
       ))}
     </Stack>
--- src/components/SparkStudio/Home/Post/Description/index.jsx
+++ src/components/SparkStudio/Home/Post/Description/index.jsx
@@ -1,11 +1,13 @@
 import LikedBookmarked from "./LikedBookmarked";
 import Price from "./Price";
+import Text from "./Text";
 
 const Description = () => {
   return (
     <>
       <LikedBookmarked />
       <Price />
+      <Text />
     </>
   );
 };
