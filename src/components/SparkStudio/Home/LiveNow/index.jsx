8fd7847ea48f9a4187995a26df718ff3b249a0b9
Gaurav Singh <gauravnumber@gmail.com>
2022-05-13 11:21:35 +0530

    slide on progress

--- src/components/SparkStudio/Home/LiveNow/index.jsx
+++ src/components/SparkStudio/Home/LiveNow/index.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { TvOutlined } from "@mui/icons-material";
+import { TvOutlined, Visibility } from "@mui/icons-material";
 import {
+  IconButton,
   Box,
   ListItem,
   ListItemIcon,
   ListItemText,
   Typography,
   useTheme,
+  Grid,
 } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import SwipeableViews from "react-swipeable-views";
@@ -67,13 +69,92 @@ const CurrentLives = () => {
               mb: "10px",
               borderRadius: "10px",
               background:
-                "linear-gradient(180deg, rgba(182, 182, 182, 0.79) 0%, rgba(11, 11, 11, 0.65) 100%)",
+                "linear-gradient(180deg, rgba(182, 182, 182, 0.79) 0%, rgba(11, 11, 11, 0.65) 100%) , url(https://source.unsplash.com/324x190)",
             }}
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
-            nobis ullam facilis cupiditate eveniet rerum, eligendi doloremque
-            rem quidem quae voluptas. Ipsam ea et quasi animi, id similique
-            eligendi hic!
+            <Grid
+              container
+              sx={{
+                p: "10px 10px",
+              }}
+            >
+              <Grid
+                item
+                container
+                direction="row"
+                // alignItems="flex-start"
+                // alignItems="flex-end"
+                // alignItems="center"
+                // justifyContent="flex-end"
+              >
+                <Grid item xs>
+                  <Typography
+                    sx={{
+                      fontSize: 16,
+                      fontWeight: 600,
+                      color: "primary.liveRed",
+                      "&:before": {
+                        content: '"["',
+                        color: "#000",
+                        pr: "4px",
+                      },
+                      "&:after": {
+                        content: '"]"',
+                        color: "#000",
+                        pl: "4px",
+                      },
+                    }}
+                  >
+                    Live &bull;
+                  </Typography>
+                </Grid>
+                <Grid item container xs justifyContent="flex-end">
+                  <Grid
+                    item
+                    container
+                    // direction="row"
+                    // spacing="2px"
+                    alignItems="center"
+                    justifyContent="inherit"
+                  >
+                    <IconButton
+                      sx={{
+                        p: 0,
+                        // mr: "2px",
+                        color: "#fff",
+                        // width: 14,
+                        // height: 10,
+                        "& .MuiSvgIcon-root": {
+                          width: 14,
+                          height: 16,
+                        },
+                      }}
+                    >
+                      <Visibility />
+                    </IconButton>
+                    <Typography
+                      sx={{
+                        fontSize: 13,
+                        fontWeight: 500,
+                        color: "#fff",
+                      }}
+                    >
+                      15,000
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography
+                      sx={{
+                        fontSize: 10,
+                        color: "#fff",
+                      }}
+                    >
+                      Watching
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
           </Box>
         ))}
       </SwipeableViews>
