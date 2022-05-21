43816cf686c11037c371c4cb0e818b05c7d2f713
Gaurav Singh <gauravnumber@gmail.com>
2022-05-21 13:35:05 +0530

    login added successfully

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,14 @@
 import axios from "axios"
-const baseUrl = import.meta.env.VITE_BACKEND_URL
+const baseUrl = `${import.meta.env.VITE_BACKEND_URL}/api/user`
 // const baseUrl = "http://localhost:4000/api/user/login"
 
 
 let token = null
 const setToken = newToken => { token = `bearer ${newToken}` }
-console.log(`token`, token)
+// console.log(`token`, token)
 
 const login = async (email, password) => {
-  return axios.post(baseUrl, {
+  return axios.post(`${baseUrl}/login`, {
     email, password
   }).then(response => response.data)
   // const response = await axios.post(baseUrl, {
